3c89d1d594799fc21471226a09659a6831e19714
16046473 <leeboxuan@hotmail.com>
2018-05-28 14:40:35 +0800

    Initial commit

---
+++ app/src/main/java/com/weebly/leeboxuan/taskmanager/DBHelper.java
@@ -0,0 +1,91 @@
+package com.weebly.leeboxuan.taskmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "tasks.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DESC = "description";
+    private static final String COLUMN_SECONDS = "seconds";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTaskTableSql = "CREATE TABLE " + TABLE_TASK + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT, "
+                + COLUMN_DESC + " TEXT, "
+                + COLUMN_SECONDS + " INTEGER ) ";
+
+        db.execSQL(createTaskTableSql);
+        Log.i("INFO", "Tables created");
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, "Buy milk");
+        values.put(COLUMN_DESC, "Low fat");
+        values.put(COLUMN_SECONDS, 5);
+        db.insert(TABLE_TASK, null, values);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        onCreate(db);
+    }
+
+    public long addTask(String name, String description, int seconds) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME , name);
+        values.put(COLUMN_DESC, description);
+        values.put(COLUMN_SECONDS, seconds);
+
+        long result = db.insert(TABLE_TASK, null, values);
+        db.close();
+
+        if (result == -1) {
+            Log.d("DBHelper", "Insert failed");
+        }
+
+        Log.d("SQL Insert ",""+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+    public ArrayList<String> getAllTasks() {
+        ArrayList<String> tasks = new ArrayList<String>();
+
+        String selectQuery = "SELECT " + COLUMN_ID + ","
+                + COLUMN_NAME + " , "
+                + COLUMN_DESC + " , "
+                + COLUMN_SECONDS + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String desc = cursor.getString(2);
+
+                tasks.add(id + ". " + name + "\n" + desc);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+}
---
+++ app/src/main/java/com/weebly/leeboxuan/taskmanager/MainActivity.java
@@ -0,0 +1,69 @@
+package com.weebly.leeboxuan.taskmanager;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> al;
+    ArrayAdapter aa;
+    ListView lv;
+    Button btnAddTask;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAddTask = (Button) this.findViewById(R.id.btnAddTask);
+        lv = (ListView) this.findViewById(R.id.lv);
+
+        al = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+        DBHelper db = new DBHelper(MainActivity.this);
+        al.clear();
+        al.addAll(db.getAllTasks());
+        db.close();
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moveToAddActivity = new Intent(MainActivity.this, AddActivity.class);
+                startActivityForResult(moveToAddActivity, 9);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 9){
+            lv = (ListView)findViewById(R.id.lv);
+            al = new ArrayList<String>();
+            DBHelper dbh = new DBHelper(MainActivity.this);
+            al.clear();
+            al.addAll(dbh.getAllTasks());
+            dbh.close();
+            aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+
+            lv.setAdapter(aa);
+        }
+    }
+}
